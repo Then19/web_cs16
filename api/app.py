32f7f2c93bff59e848c368249b5d2207e9ae4b82
Then19 <tol9hstar@gmail.com>
2022-04-21 14:28:52 +0500

    add CORS

--- api/app.py
+++ api/app.py
@@ -3,9 +3,23 @@ from fastapi import FastAPI, Depends
 from database import crud
 from database.database import get_db
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://api.v-cs.ru"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/server/server_list", response_model=list[schemas.ServerInfo])
