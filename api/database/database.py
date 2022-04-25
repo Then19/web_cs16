046faf28542f4d8ec4b55b958a906a3ffaffdcf9
Then19 <tol9hstar@gmail.com>
2022-04-25 12:44:11 +0500

    fix connection

--- api/database/database.py
+++ api/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from config import SQL_LOGIN
 
-engine = create_engine(SQL_LOGIN)
+engine = create_engine(SQL_LOGIN, pool_recycle=3600)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
