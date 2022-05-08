0c762922cddeb62d012f0ea6f9d2858c428e5f1e
Then19 <tol9hstar@gmail.com>
2022-05-08 11:45:42 +0500

    some fix css

--- client/src/components/stats/top/StatsLabel.jsx
+++ client/src/components/stats/top/StatsLabel.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import "./top.css"
+import {showToast} from "../../../tools/toast";
 
-const StatsLabel = ({StatsData, Title, obj, objTitle}) => {
+const StatsLabel = ({StatsData, Title, obj, objTitle, titleColor = 'title-gold'}) => {
     return (
         <div className={"body-top"}>
             <div className={"block-border-top"}/>
             <div className={"block-background-top"}/>
             <div className="top-label-block">
-                <div className={"top-label-title"}>{Title}</div>
+                <div className={"top-label-title " + titleColor}>{Title}</div>
                 <div className={"top-users-list-block-title"}>
                     <div>Name</div><div>{objTitle}</div>
                 </div>
                 {StatsData.map((item, index) =>
-                    <div key={index} className={"top-users-list-block"}>
-                        <div><a href="#">{index + 1}. {item.name}</a></div><div>{item[obj]}</div>
-                    </div>
+                    <a onClick={() => showToast('warn', 'Статистика пользователей пока не доступна')} href="#">
+                        <div key={index} className={"top-users-list-block " + "top-color-" + index}>
+                        <div>{index + 1}. {item.name}</div><div>{item[obj]}</div>
+                    </div></a>
                 )}
             </div>
         </div>
